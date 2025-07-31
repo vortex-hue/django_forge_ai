bb2b2a9389fb80176206319858c56d6d8647fdfe
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-07-31 18:31:26 +0100

    ci: add automated testing and deployment pipelines

--- django_forge_ai/__init__.py
+++ django_forge_ai/__init__.py
@@ -5,4 +5,5 @@ DjangoForgeAI - A comprehensive AI toolbox for Django
 __version__ = "0.1.0"
 __author__ = "DjangoForgeAI Contributors"
 
-default_app_config = "django_forge_ai.apps.DjangoForgeAIConfig" 
+default_app_config = "django_forge_ai.apps.DjangoForgeAIConfig"
+# Improve
